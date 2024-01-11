7486a26ea89270e608658db7ba7347b772e7ddf2
Mahip Adhikari <mahip.adhikari4@gmail.com>
2024-01-11 21:38:33 +0545

    comma and okay

---
+++ 03userinput/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "welcome to user input"
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println(welcome)
+
+	fmt.Println("enter the rating for our pizza: ")
+
+	// comma okay syntax || err ok
+
+	input, err := reader.ReadString('\n')
+	fmt.Println("thanks for rating", input)
+	fmt.Println("the error is", err)
+
+}
