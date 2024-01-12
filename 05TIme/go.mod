acf08a72d107d284d1540c24cb426d813c026205
Mahip Adhikari <mahip.adhikari4@gmail.com>
2024-01-12 22:54:13 +0545

    conversion of string and a little bit of time

---
+++ 05TIme/go.mod
@@ -0,0 +1,3 @@
+module Time
+
+go 1.21.5
---
+++ 05TIme/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to time study of golang")
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+}
